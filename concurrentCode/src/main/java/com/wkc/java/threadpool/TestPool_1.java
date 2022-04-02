48ed873c2e4bad8a86cad2ab237ee3d0419d1260
weikaichen <2993821053@qq.com>
2022-04-02 13:58:16 +0800

    pool

--- concurrentCode/src/main/java/com/wkc/java/threadpool/TestPool_1.java
+++ concurrentCode/src/main/java/com/wkc/java/threadpool/TestPool_1.java
@@ -21,17 +21,58 @@ class BlockingQueue<T>{
     private ReentrantLock lock=new ReentrantLock();
 
     //3.生产者条件变量
-    private Condition proWaitSet=lock.newCondition();
+    private Condition fullWaitSet=lock.newCondition();
 
     //4.消费者条件变量
-    private Condition comWaitSet=lock.newCondition();
+    private Condition emptyWaitSet=lock.newCondition();
 
     //5.队列容量
     private int capacity;
 
     //阻塞获取
-
+    public T take(){
+        lock.lock();
+        try {
+            while (queue.isEmpty()){
+                try {
+                    emptyWaitSet.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T t = queue.removeFirst();
+            fullWaitSet.signalAll();
+            return t;
+        }finally {
+            lock.unlock();
+        }
+    }
 
     //阻塞添加
+    public void put(T t){
+        lock.lock();
+        try {
+            while (queue.size()==capacity){
+                try {
+                    fullWaitSet.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            queue.addLast(t);
+            emptyWaitSet.signalAll();
 
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public int size(){
+        lock.lock();
+        try {
+            return queue.size();
+        }finally {
+            lock.unlock();
+        }
+    }
 }
