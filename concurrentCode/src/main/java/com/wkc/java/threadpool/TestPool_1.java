cff359e7171604270dc98784ecf3b9cdf09c7b52
weikaichen <2993821053@qq.com>
2022-04-02 14:48:43 +0800

    tp

--- concurrentCode/src/main/java/com/wkc/java/threadpool/TestPool_1.java
+++ concurrentCode/src/main/java/com/wkc/java/threadpool/TestPool_1.java
@@ -2,6 +2,8 @@ package com.wkc.java.threadpool;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,7 +14,70 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class TestPool_1 {
 }
+class ThreadPool{
+    private BlockingQueue<Runnable> taskQueue;
 
+    private HashSet<Worker> workers=new HashSet<>();
+
+    //核心线程数
+    private int coreSize;
+
+    //获取任务的超时时间
+    private long timeout;
+
+    //时间单位
+    private TimeUnit timeUnit;
+
+    //执行任务
+    public void execute(Runnable task){
+        //当任务数没有超过coreSize，直接交给worker
+        //超过coreSize，加入任务队列
+        synchronized (workers){
+            if (workers.size()<coreSize){
+                Worker worker = new Worker(task);
+                workers.add(worker);
+                worker.start();
+            }else {
+                taskQueue.put(task);
+            }
+        }
+    }
+
+    public ThreadPool(int coreSize, long timeout, TimeUnit timeUnit,int capacity) {
+        this.coreSize = coreSize;
+        this.timeout = timeout;
+        this.timeUnit = timeUnit;
+        taskQueue=new BlockingQueue<>(capacity);
+    }
+
+    class Worker extends Thread{
+        private Runnable task;
+
+        public Worker(Runnable task) {
+            this.task = task;
+        }
+
+        @Override
+        public void run() {
+            //执行任务
+            //1.当task不为空，执行任务
+            //2.当task执行完毕，从任务队列获取任务执行
+            while (task!=null||(task=taskQueue.poll(timeout, timeUnit))!=null){
+                try {
+                    task.run();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    task=null;
+                }
+            }
+            synchronized (workers){
+                workers.remove(this);
+            }
+        }
+    }
+
+}
 class BlockingQueue<T>{
     //1.任务队列
     private Deque<T> queue=new ArrayDeque<>();
@@ -26,8 +91,39 @@ class BlockingQueue<T>{
     //4.消费者条件变量
     private Condition emptyWaitSet=lock.newCondition();
 
+
     //5.队列容量
     private int capacity;
+
+    public BlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    //超时获取
+    public T poll(long timeout,TimeUnit timeUnit){
+        lock.lock();
+        try {
+            long nanos = timeUnit.toNanos(timeout);
+            while (queue.isEmpty()){
+                try {
+                    //需要考虑虚假唤醒问题
+                    if (nanos<=0){
+                        return null;
+                    }
+                    nanos = emptyWaitSet.awaitNanos(nanos);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T t = queue.removeFirst();
+            fullWaitSet.signalAll();
+            return t;
+        }finally {
+            lock.unlock();
+        }
+    }
+
 
     //阻塞获取
     public T take(){
